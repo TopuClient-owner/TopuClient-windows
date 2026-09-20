c640ef9bf195b5c2e0fe0eb550e0b0b5de21c816
TopuOrganization <topuclient@gmail.com>
2026-09-20 20:43:08 +0500

    Fix Microsoft account display for current Xbox account API

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -916,7 +916,7 @@ private static readonly HttpClient Http = CreateHttpClient();
                                 .OfType<IXboxGameAccount>())
                     {
                         string username =
-                            account.XboxTokens?.XstsToken?.XuiClaims?.Gamertag
+                            account.Identifier
                             ?? "Microsoft Account";
 
                         AccountSelector.Items.Add(
